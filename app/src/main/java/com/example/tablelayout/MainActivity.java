2822e2c29ac9af8dc1f622006e23b240f3cff3c3
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-01 11:34:20 +0200

    Creation des fragment

--- app/src/main/java/com/example/tablelayout/MainActivity.java
+++ app/src/main/java/com/example/tablelayout/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.tablelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //On Instencie le FragmentManger
+    FragmentManager fragmentManager = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+
     }
 }
